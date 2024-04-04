5190e46b65e67c379b4f5876cdc12c518b57ff30
Sudhan1112 <sudhansudhan83@gmail.com>
2024-04-04 16:22:13 +0530

    Added structure to the about page

--- Frontend/Productivity-Panda-MainPage/src/Components/MainPage/LandingPage.jsx
+++ Frontend/Productivity-Panda-MainPage/src/Components/MainPage/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../../src/index.css';
 import NavBar from './NavBar';
 import NavHomeBTN from './NavHomeBTN';
+import NavAboutPage from './NavAboutPage';
 
 function LandingPage() {
   return (
     <>
      <NavBar/>
     <NavHomeBTN/>
+    <NavAboutPage/>
     </>
   )
 }
--- Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavAboutPage.jsx
+++ Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavAboutPage.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function NavAboutPage() {
   return (
-    <div>NavAboutPage</div>
+    <>
+    <div style={{textAlign:'center',marginBottom:"10px"}}>
+        <p>About</p>
+    </div>
+    <div style={{textAlign:'center',fontWeight:"bold",marginBottom:"10px"}}>
+        <p> 
+        "Welcome to Productive Panda, where we transform your day, one task at a time. Our platform streamlines your workflow, empowering you to achieve goals effortlessly. We prioritize seamless user experience, ensuring adaptability to your needs. From task creation to collaboration, our feature-rich app covers it all.
+        Real-time communication keeps you connected throughout your journey. Our mission is clear: to exceed expectations and support your success.
+        </p>
+    </div>
+    <div style={{textAlign:'center' ,marginTop:"10px"}}>
+    Join us at Productive Panda, where productivity meets simplicity."
+    </div>
+
+    <div style={{fontWeight:'bold'}}>
+        Features
+    </div>
+
+
+    </>
   )
 }
 
