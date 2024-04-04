30d414a29397375447476b31371824d96fd2e4f8
Sudhan1112 <sudhansudhan83@gmail.com>
2024-04-04 15:47:27 +0530

    Created getstarted page and given alignment

--- Frontend/Productivity-Panda-MainPage/src/App.jsx
+++ Frontend/Productivity-Panda-MainPage/src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import React from 'react'
 import LandingPage from './Components/MainPage/LandingPage';
 // import MainPage from './Components/MainPage/MainPage'
-// import NavBar from './Components/LandingPage/NavBar'
 
 function App() {
   return(
     <>
     {/* <MainPage/> */}
     <LandingPage/>
-    {/* <NavBar/> */}
     </>
   )
 }
---
+++ Frontend/Productivity-Panda-MainPage/src/Components/NavHomeBTN.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import RisingUpArrow from "../assets/images/Rising-up-arrow-BG.png"
+
+function NavHomeBTN() {
+  return (
+    <>
+    <div className='Overall-div-of-get-started-page'>
+        <img src={RisingUpArrow} alt="risingUpArrow" />
+        <div className='Slogan-and-tagline-of-get-started-page'>
+        <p className='slogan-para'>Stay-Focused</p>
+        <p className='slogan-para'>Stay-Productive with</p>
+        <p className='slogan-para'>Productivity Panda</p>
+        <p className='tagline-of-the-landing-page'>Transform Your Day, Boost Your Productivity, One Task at a Time: Productive Panda</p>
+        </div>
+        <button>Get Started</button>
+    </div>
+    </>
+  )
+}
+
+export default NavHomeBTN
