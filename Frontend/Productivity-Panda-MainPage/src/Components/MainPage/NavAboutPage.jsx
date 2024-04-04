ba617c4e5b8f9ff61174d960eab3a4e1e7fb484d
Sudhan1112 <sudhansudhan83@gmail.com>
2024-04-04 16:25:03 +0530

    Added structure to the about page and style modification is given

--- Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavAboutPage.jsx
+++ Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavAboutPage.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function NavAboutPage() {
   return (
     <>
-    <div style={{textAlign:'center',marginBottom:"10px"}}>
+    <div style={{textAlign:'left',marginBottom:"12px"}}>
         <p>About</p>
     </div>
-    <div style={{textAlign:'center',fontWeight:"bold",marginBottom:"10px"}}>
+    <div style={{textAlign:'left',fontWeight:"bold",marginBottom:"14px"}}>
         <p> 
         "Welcome to Productive Panda, where we transform your day, one task at a time. Our platform streamlines your workflow, empowering you to achieve goals effortlessly. We prioritize seamless user experience, ensuring adaptability to your needs. From task creation to collaboration, our feature-rich app covers it all.
         Real-time communication keeps you connected throughout your journey. Our mission is clear: to exceed expectations and support your success.
         </p>
     </div>
-    <div style={{textAlign:'center' ,marginTop:"10px"}}>
+    <div style={{textAlign:'center' ,marginTop:"12px"}}>
     Join us at Productive Panda, where productivity meets simplicity."
     </div>
 
