9ad266694f54fcaadf5875bf0a882dc0b7238eaf
Sudhan1112 <sudhansudhan83@gmail.com>
2024-04-06 17:08:21 +0530

    Added features and about page for the application

--- Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavAboutPage.jsx
+++ Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavAboutPage.jsx
@@ -1,26 +1,56 @@
 import React from 'react'
+import Authentication_img_BG from "../../assets/images/Authentication img transparent.png"
+import Trusted_and_secure_img_BG from "../../assets/images/Secure and trusted browsing transparent.png"
+import Mail_img_BG from "../../assets/images/Mail img BG.png";
+import calendar_img from "../../assets/images/Calendar img BG.png"
+import group_img from "../../assets/images/Group img BG.png"
+import extension_img from "../../assets/images/Extension img BG.png"
 
 function NavAboutPage() {
   return (
     <>
-    <div style={{textAlign:'left',marginBottom:"12px"}}>
-        <p>About</p>
-    </div>
-    <div style={{textAlign:'left',fontWeight:"bold",marginBottom:"14px"}}>
-        <p> 
+    <div className='About-page' style={{backgroundColor: "white"}}>
+        <p className='about-P-tag'>About me</p>
+        <p className='Description-p-tag-of-about-me'> 
         "Welcome to Productive Panda, where we transform your day, one task at a time. Our platform streamlines your workflow, empowering you to achieve goals effortlessly. We prioritize seamless user experience, ensuring adaptability to your needs. From task creation to collaboration, our feature-rich app covers it all.
         Real-time communication keeps you connected throughout your journey. Our mission is clear: to exceed expectations and support your success.
         </p>
-    </div>
-    <div style={{textAlign:'center' ,marginTop:"12px"}}>
-    Join us at Productive Panda, where productivity meets simplicity."
-    </div>
-
-    <div style={{fontWeight:'bold'}}>
+        <p className='Description-p-tag-of-about-me'>
+          Join us at Productive Panda, where productivity meets simplicity."
+        </p>
+        <button className='Join-Now-btn'>Join Now!</button>
+      <p className='features-p-tag-of-about-me'>
         Features
-    </div>
-
-
+        </p>
+        <div className='Features-container-1'>
+          <div className='Authetication_container'>
+            <img src={Authentication_img_BG} alt="Authentication_BG" />
+            <p>Integration with Third party authentication</p>
+          </div>
+          <div className='Reliable_Container'>
+            <img src={Trusted_and_secure_img_BG} alt="Trusted_img_BG" />
+            <p>Safe and Secure trusted user experience</p>
+          </div>
+          <div className='Mail_container'>
+            <img src={Mail_img_BG} alt="Mail_img_BG" />
+            <p>Sending notifications to the user's email</p>
+          </div>
+          </div>
+          <div className='Features-container-2'>
+            <div className='calendar_container'>
+              <img src={calendar_img} alt="calendar_img" />
+              <p>Adding tasks to the user's Google Calendar</p>
+            </div>
+            <div className='Group-img_container'>
+              <img src={group_img} alt="group_img" />
+              <p>Facilitating group communication within projects for multiple users</p>
+            </div>
+            <div className='extension_container'>
+              <img src={extension_img} alt="extension_img" />
+              <p>Offering a browser extension for enhanced accessibility</p>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
