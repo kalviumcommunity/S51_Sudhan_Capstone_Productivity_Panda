702e5b4c09efe28b801b41e096f1297afa66dc4d
Sudhan1112 <sudhansudhan83@gmail.com>
2024-04-10 15:25:10 +0530

    Created a animated NavBar with hamburger symbol

--- Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavBar.jsx
+++ Frontend/Productivity-Panda-MainPage/src/Components/MainPage/NavBar.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductivityPandaImage from '../../assets/images/logo-productivity-panda-transparent.png';
 
+
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <>
-    <nav className='NavBarContainer'>
-      <div className='ProductivityPandaImage'><img src={ProductivityPandaImage} alt="Logo" /></div>
-      <ul>
+    <nav className={`NavBarContainer ${isMenuOpen ? 'CrossSymbol' : 'open'}`}>
+      <div className='ProductivityPandaImage'>
+        <img src={ProductivityPandaImage} alt="Logo" />
+      </div>
+      <div className="HamburgerMenu" onClick={toggleMenu}>
+        <div className={`HamburgerLine ${isMenuOpen ? 'line1' : ''}`}></div>
+        <div className={`HamburgerLine ${isMenuOpen ? 'line2' : ''}`}></div>
+        <div className={`HamburgerLine ${isMenuOpen ? 'line3' : ''}`}></div>
+      </div>
+      <ul className={`${isMenuOpen ? 'open' : ''}`}>
         <li>Home</li>
         <li>About</li>
         <li>Features</li>
@@ -15,8 +29,7 @@ function NavBar() {
         <li>Sign-Up</li>
       </ul>
     </nav>
-    </>
-  )
+  );
 }
 
 export default NavBar;
