90b1968b7f1f557fc0632b1c87f513e4b08dfad9
Sudhan1112 <sudhansudhan83@gmail.com>
2024-04-25 10:39:34 +0530

    Sign-up post request is done but error handling is in progress

---
+++ Backend/Validators/SignUpValidator.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const signUpValidator = Joi.object({
+    Username: Joi.string().min(3).max(20).required().label('Username'),
+    Email: Joi.string().allow('').required().label('Email'),
+    Password : Joi.string().min(6).max(12).required().label("Password")
+})
+
+
+const ValidatingSignUp = (data) => {
+    return signUpValidator.validate(data, {abortEarly: false})
+}
+
+module.exports = ValidatingSignUp;
